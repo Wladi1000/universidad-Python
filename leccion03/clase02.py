078ea89ef3f61e99616fcb4391e2e7083c9d8052
Wladimir Sanvicente <wladimirss14@gmail.com>
2023-03-27 23:36:21 -0400

    Clase02

---
+++ leccion03/clase02.py
@@ -0,0 +1,12 @@
+# Cadena (String)
+miGrupoFavorito = 'Avenged Sevenfold';
+comentario = 'The Best Metal Band';
+# Concatenacion de cadenas
+print("Mi grupo favorito es: " + miGrupoFavorito + ' ' + comentario);
+print("Mi grupo favorito es:", miGrupoFavorito, comentario); # Agrega un espacio de forma automatica con ","
+
+# Truncamiento
+numero1 = '1';
+numero2 = '2';
+print("Concatenacion:", numero1, numero2);
+print("Suma:", int(numero1) + int(numero2));
