16c71ad4ced85f71dff3c8235be3810660f78d24
Wladimir Sanvicente <wladimirss14@gmail.com>
2023-03-27 23:42:24 -0400

    Clase04

---
+++ leccion03/clase04.py
@@ -0,0 +1,5 @@
+# Funcion input para procesar la entrada del usuario
+print('Ingrese el numero que quiere imprimir por pantalla');
+resultado = input();
+print('El nro es', resultado);
+print('Fin del programa');
