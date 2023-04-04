d165510ca20cecf132936a31aeea22a3dc0d966a
Wladimir Sanvicente <wladimirss14@gmail.com>
2023-04-03 23:56:15 -0400

    Ejercicio05

---
+++ leccion04/ejercicio05.py
@@ -0,0 +1,12 @@
+# Asistir a juego
+vacaciones = False;
+diaDescanso = False;
+
+print('Bienvenido al programa de ver si puedes asistir al juego');
+
+if ( vacaciones or diaDescanso ):
+  print('Puede asistir al juego');
+else:
+  print('No podra asistir al juego');
+
+print('Fin de programa');
