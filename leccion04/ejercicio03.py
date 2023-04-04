ff3c9ca73b24bfb9a739c9066eeec76b0a2fde3b
Wladimir Sanvicente <wladimirss14@gmail.com>
2023-04-03 23:41:45 -0400

    ejercicio03

---
+++ leccion04/ejercicio03.py
@@ -0,0 +1,10 @@
+# Verificar si una persona es mayor de edad
+print('Bienvenido al programa para saber si una persona es mayor de edad!');
+edad = int(input('Ingrese su edad para evaluar: '));
+
+if ( edad >= 18 ):
+  print('Usted es mayor de edad');
+else:
+  print('Usted no es mayor de edad');
+
+print('Fin de programa');
