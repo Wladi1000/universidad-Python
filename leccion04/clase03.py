73d9ad049b65e0325025fdc3d9be1e60ccfef578
Wladimir Sanvicente <wladimirss14@gmail.com>
2023-04-03 23:27:55 -0400

    Clase 03

---
+++ leccion04/clase03.py
@@ -0,0 +1,18 @@
+# Asignacion
+miVariable = 10;
+print( miVariable );
+miVariable = miVariable + 1;
+print( miVariable );
+
+# Incremento con reasignacion
+miVariable += 5;
+print( miVariable );
+
+miVariable -= 7;
+print( miVariable );
+
+miVariable *= 3;
+print( miVariable );
+
+miVariable /= 27;
+print( miVariable );
