351ef297154a2501f022c3d5366ee32abec46f31
Wladimir Sanvicente <wladimirss14@gmail.com>
2023-04-22 15:33:44 -0400

    Ejercicio 02

---
+++ leccion06/ejercicio02.py
@@ -0,0 +1,7 @@
+# Imprimir los números enteros del 5 al 1 utilizando un ciclo while
+maximo = 1;
+contador = 5;
+while contador >= maximo:
+  print(contador);
+  # contador = contador + 1
+  contador -= 1;
